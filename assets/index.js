b381f21b5631c16cae4b72845714c79e18a77f95
pando <spreadpando@gmail.com>
2019-04-27 10:11:58 -0600

    init js / firebase / html

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+var config = {
+	apiKey: "AIzaSyDERgq2wOhamH6XLkqcothJsDxW35IMSJs",
+	authDomain: "spreadpando-102.firebaseapp.com",
+	databaseURL: "https://spreadpando-102.firebaseio.com",
+	projectId: "spreadpando-102",
+	storageBucket: "spreadpando-102.appspot.com",
+	messagingSenderId: "982902885400"
+};
+firebase.initializeApp(config);
