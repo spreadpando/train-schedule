b3857bdf1b9a2c862f257db23dca673491e7de42
pando <spreadpando@gmail.com>
2019-04-28 17:27:14 -0600

    functionalit complete

--- assets/index.js
+++ assets/index.js
@@ -7,3 +7,49 @@ var config = {
 	messagingSenderId: "982902885400"
 };
 firebase.initializeApp(config);
+let dB = firebase.database();
+
+$('#submit').on('click', function () {
+	event.preventDefault();
+	let name = $('#name').val().trim();
+	let destination = $('#dest').val().trim();
+	let initTime = $('#init-time').val().trim();
+	let frequency = $('#freq').val().trim();
+	let Train = {
+		"name": name,
+		"destination": destination,
+		"initial": initTime,
+		"frequency": frequency,
+	}
+	dB.ref('/trainsArray').push({
+		Train
+	});
+})
+
+dB.ref("/trainsArray").on('value', function (snapshot) {
+	renderTrains(snapshot);
+});
+
+function renderTrains(snapshot) {
+	$('#schedule').html('');
+	snapshot.forEach(function (childSnapshot) {
+		let name = childSnapshot.val().Train.name;
+		let nameDiv = $('<div>').attr('class', 'row').html("The <h1>" + name + "</h1>");
+		let destination = childSnapshot.val().Train.destination;
+		let destDiv = $('<div>').attr('class', 'row').html(" to <h1>" + destination + "</h1>");
+		let initial = childSnapshot.val().Train.initial;
+		let frequency = childSnapshot.val().Train.frequency;
+		let detail = $('<div>').attr('class', 'detail');
+		let firstTrain = moment(initial, 'hh:mm').subtract(1, 'y');
+		let difference = moment().diff(moment(firstTrain), 'minutes');
+		let remainder = difference % frequency;
+		let nextTrain = frequency - remainder;
+		let nextDiv = $('<div>').attr('class', 'roww').html(" will be arriving in <h1>" + nextTrain + " minute(s) </h1>");
+		let arriving = moment().add(nextTrain).format("HH:mm");
+		let arrivalDiv = $('<div>').attr('class', 'row').html(" at <h1>" + arriving + "</h1>");
+		detail.append(nameDiv, destDiv, nextDiv, arrivalDiv);
+		$('#schedule').append(detail);
+
+		// ...
+	});
+};
